97688798915b2164f4df21f5a07e9c638f22ee40
FurkannByrm <frknbyrm05@gmail.com>
2024-11-12 14:52:37 +0300

    serial port

---
+++ robotics_demos/serial_cmm/build/CMakeFiles/serial_read.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/serial_read.dir/src/serial_read.cpp.o"
+  "CMakeFiles/serial_read.dir/src/serial_read.cpp.o.d"
+  "serial_read"
+  "serial_read.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/serial_read.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
